5fa852cb07e362888c8e75f2c97a43c7b72dfdbe
bahaa-fayed <prog.bahaa@gmail.com>
2026-07-15 09:39:27 +0300

    Refactor project to feature-based architecture

---
+++ app/dashboard/routes.py
@@ -0,0 +1,9 @@
+from flask import render_template
+
+from . import dashboard_bp
+
+
+@dashboard_bp.route("/")
+def index():
+
+    return render_template("dashboard/index.html")
---
+++ app/myscript.sh
@@ -0,0 +1,5 @@
+git add .
+
+git commit -m "Refactor project to feature-based architecture"
+
+git push
